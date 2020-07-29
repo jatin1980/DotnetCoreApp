4f32f08c4c53540de1f1f1aec3641d0d7909cd91
jatin1980 <jatin.ahir1980@gmail.com>
2020-07-29 15:00:38 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
